56a7556f7f92dd26ea6a6ba3f6b76fec0239e41e
demandre <joris.demandre@gmail.com>
2019-02-27 10:16:31 +0100

    linterize code

--- src/app.js
+++ src/app.js
@@ -2,10 +2,10 @@ var Credentials = require('./credentials');
 var Twitter = require('twitter');
 
 var client = new Twitter({
-  consumer_key: Credentials.TWITTER_consumer_key,
-  consumer_secret: Credentials.TWITTER_consumer_secret,
-  access_token_key: Credentials.TWITTER_access_token_key,
-  access_token_secret: Credentials.TWITTER_access_token_secret
+  'consumer_key': Credentials.TWITTER_consumer_key,
+  'consumer_secret': Credentials.TWITTER_consumer_secret,
+  'access_token_key': Credentials.TWITTER_access_token_key,
+  'access_token_secret': Credentials.TWITTER_access_token_secret
 });
 
 var app = require('express')();
@@ -16,15 +16,15 @@ var tweet = null;
 
 server.listen(3080);
 
-io.on('connection', function (socket) {
-  socket.on('search', function (keyword) {
+io.on('connection', function onConnection (socket) {
+  socket.on('search', function onSearch (keyword) {
     console.log(keyword);
     if (stream) {
       stream.destroy();
     }
 
     stream = client.stream('statuses/filter', {'track': keyword});
-    stream.on('data', function(event) {
+    stream.on('data', function onData (event) {
       tweet = {
         'created_at': event.created_at,
         'id_str': event.id_str,
@@ -41,7 +41,7 @@ io.on('connection', function (socket) {
       };
       socket.emit('tweet', tweet);
     });
-    stream.on('error', function(error) {
+    stream.on('error', function onError (error) {
       console.log(error);
     });
   });
